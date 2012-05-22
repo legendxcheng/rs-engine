3d5a9c5689c38fcc1830d1dc71b97fe01099ced4
legendxcheng@gmail.com <legendxcheng@gmail.com@fe446340-a858-f71d-eadb-cde6dcdf4678>
2012-05-22 13:49:44 +0000

    use FMOD as Audio Library to Use FMOD correctly, you should copy fmodex.dll into C:/Windows/System

--- RSEngine/SoundClass.cpp
+++ RSEngine/SoundClass.cpp
@@ -14,17 +14,46 @@ SoundClass::SoundClass(const SoundClass& other)
 SoundClass::~SoundClass()
 {
 }
+
+void SoundClass::UpdateFrame()
+{
+	m_system->update();
+}
+
 bool SoundClass::Initialize(HWND hwnd)
 {
-	
+	FMOD_RESULT result;
+	result = FMOD::System_Create(&m_system);
+	ERRCHECK(result);
+
+	result = m_system->init(MAX_CHANNEL_NUM, FMOD_INIT_NORMAL, 0);
+	ERRCHECK(result);
+
 
 	return true;
 }
 
 void SoundClass::Shutdown()
 {
-
+	// We do not have to care about channels, FMOD will care about it for us.
+	for (std::vector<FMOD::Sound*>::iterator iter = m_soundList.begin(); iter != m_soundList.end(); ++iter)
+	{
+		(*iter)->release();
+	}
 
 	return;
 }
 
+void SoundClass::ERRCHECK(FMOD_RESULT result)
+{
+	if (result != FMOD_OK)
+	{
+		printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+		exit(-1);
+	}
+}
+
+void SoundClass::InitializeResource()
+{
+
+}
